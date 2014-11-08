0eaefc2c5138e20202538148991e84181882142a
PLkolek <mateusz.kolaczek@gmail.com>
2014-11-08 16:57:59 +0100

    Some work on posts in admin panel

---
+++ app/controllers/admin/posts_controller.rb
@@ -0,0 +1,60 @@
+class Admin::PostsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  respond_to :html
+
+  def index
+    @posts = Post.all
+    respond_with(@posts)
+  end
+
+  def show
+    respond_with(@post)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to admin_posts_url, notice: 'Post was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to admin_posts_url, notice: 'Post was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to admin_posts_url, notice: 'Post was successfully destroyed.' }
+    end
+  end
+
+  private
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content, :created_at)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -64,5 +64,6 @@ Rails.application.routes.draw do
   namespace :admin do
     get '', to: 'dashboard#index', as: '/'
     resources :posts
+    resources :comments
   end
 end

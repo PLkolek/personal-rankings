491144f85c4a289a0397206334044ff4c455f837
PLkolek <mateusz.kolaczek@gmail.com>
2014-11-10 19:46:53 +0100

    Rank column on posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141110163454) do
+ActiveRecord::Schema.define(version: 20141110183407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20141110163454) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "category_id"
+    t.float    "rank"
   end
 
   add_index "posts", ["category_id"], name: "index_posts_on_category_id", using: :btree
